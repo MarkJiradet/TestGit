2eec27d1c03f172c98308e5d283877e347f13efd
MarkJiradet <jiradetn@gmail.com>
2021-11-27 03:06:40 +0700

    Create Lab07_3.c

---
+++ Lab07_3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    char a1,a2,a3,a4,a5;
+
+    printf("Enter a number : ");
+    scanf("\n%c%c%c%c%c",&a1,&a2,&a3,&a4,&a5);
+    printf("output string : %c%c%c%c%c",a1+16,a2+16,a3+16,a4+16,a5+16);
+    printf("Test GithubOhbro");
+}
